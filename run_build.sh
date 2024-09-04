acdb6edd0d7bec443ead63ab00947f372baa5fe9
Philipp Kempski <pkempski@login13.frontier.olcf.ornl.gov>
2024-09-04 12:05:54 -0400

    Merged with branch particlesPK_turbulence

---
+++ run_build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+module restore
+module load PrgEnv-cray
+module load craype-accel-amd-gfx90a
+module load cpe/23.12
+module load cce/17.0.0
+module load rocm/5.7.1
+module load cmake cray-python emacs
+module unload darshan-runtime
+export MPICH_GPU_SUPPORT_ENABLED=1
+export FI_MR_CACHE_MONITOR=disabled #memhooks
+export FI_MR_CACHE_MAX_COUNT=0  # libfabric disable caching
+export MPICH_SMP_SINGLE_COPY_MODE=NONE
+export FI_CXI_RX_MATCH_MODE=software
+
+
+athenak='/ccs/home/pkempski/athenak-PK'
+build='/ccs/home/pkempski/athenak-PK/build'
+
+cd ${athenak}
+cmake -Bbuild -DAthena_ENABLE_MPI=ON -DKokkos_ARCH_ZEN3=ON -DKokkos_ARCH_VEGA90A=ON \
+      -DKokkos_ENABLE_HIP=ON -DCMAKE_CXX_COMPILER=CC \
+      -DCMAKE_EXE_LINKER_FLAGS="-L${ROCM_PATH}/lib -lamdhip64" \
+      -DCMAKE_CXX_FLAGS="-I${ROCM_PATH}/include "  -DPROBLEM=part_static_turb   # -munsafe-fp-atomics" \
+cd ${build}
+make -j
