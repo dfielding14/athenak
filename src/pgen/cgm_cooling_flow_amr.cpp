58c960b593df5274c16421baed7c68d3035ee5d2
Zun Yi Brent Tan <zunyibrent@login10.frontier.olcf.ornl.gov>
2025-06-13 12:07:14 -0400

    update cooling/heating

--- src/pgen/cgm_cooling_flow_amr.cpp
+++ src/pgen/cgm_cooling_flow_amr.cpp
@@ -21,6 +21,15 @@
 //                               Globals                                     //
 //===========================================================================//
 
+KOKKOS_INLINE_FUNCTION 
+void SetEquilibriumState(const DvceArray5D<Real> &u0, 
+  int m, int k, int j, int i, 
+  Real x1v, Real x2v, Real x3v, 
+  Real x1l, Real x1r, Real x2l, Real x2r, 
+  Real G, Real r_s, Real rho_s, Real m_g, 
+  Real a_g, Real z_g, Real r_m, Real rho_m,
+  Real gm1, const ProfileReader &disk_profile);
+    
 KOKKOS_INLINE_FUNCTION
 void SetCoolingFlowState(const DvceArray5D<Real> &u0, 
                          int m, int k, int j, int i, 
@@ -41,9 +50,9 @@ Real GravPot(Real x1, Real x2, Real x3,
 
 void UserSource(Mesh* pm, const Real bdt);
 void GravitySource(Mesh* pm, const Real bdt);
-void MassLossSource(Mesh* pm, const Real bdt);
 void UserBoundary(Mesh* pm);
 void FreeProfile(ParameterInput *pin, Mesh *pm);
+void RefinementCondition(MeshBlockPack* pmbp);
 
 namespace {
   // Constants for gravitational potential
@@ -55,10 +64,6 @@ namespace {
   Real r_200;
   Real rho_mean;
 
-  // Constants for mass removal
-  Real mass_loss_rate;
-  Real mass_loss_radius;
-
   // Constants for rotation
   Real r_circ;
   Real v_circ;
@@ -66,6 +71,9 @@ namespace {
   // Add profile reader and flag
   ProfileReaderHost profile_reader_host;  // Host-side reader
   ProfileReader profile_reader;           // Device-side reader
+
+  ProfileReaderHost disk_profile_reader_host;  // Host-side reader
+  ProfileReader disk_profile_reader;           // Device-side reader
 }
 
 //===========================================================================//
@@ -80,6 +88,7 @@ void ProblemGenerator::UserProblem(ParameterInput *pin, const bool restart) {
   user_srcs_func  = UserSource;
   user_bcs_func   = UserBoundary;
   pgen_final_func = FreeProfile;
+  //user_ref_func  = RefinementCondition;
 
   if (restart) return;
 
@@ -91,9 +100,6 @@ void ProblemGenerator::UserProblem(ParameterInput *pin, const bool restart) {
   z_gal     = pin->GetReal("potential", "z_gal");
   r_200     = pin->GetReal("potential", "r_200");
   rho_mean  = pin->GetReal("potential", "rho_mean");
-
-  mass_loss_rate = pin->GetReal("problem", "mass_loss_rate");
-  mass_loss_radius = pin->GetReal("problem", "mass_loss_radius");
 
   r_circ = pin->GetReal("problem", "r_circ");
   v_circ = pin->GetReal("problem", "v_circ");
@@ -109,6 +115,17 @@ void ProblemGenerator::UserProblem(ParameterInput *pin, const bool restart) {
     std::cerr << "Error loading profiles: " << e.what() << std::endl;
   }
 
+  // Read the disk profile file
+  std::string disk_profile_file = pin->GetString("problem", "disk_profile_file");
+  try {
+    disk_profile_reader_host.ReadProfiles(disk_profile_file);
+    // Create device-accessible reader
+    disk_profile_reader = disk_profile_reader_host.CreateDeviceReader();
+    std::cout << "Successfully loaded disk profiles from " << disk_profile_file << std::endl;
+  } catch (const std::exception& e) {
+    std::cerr << "Error loading disk profiles: " << e.what() << std::endl;
+  }
+
   // Capture variables for kernel
   int &is = indcs.is; int &ie = indcs.ie;
   int &js = indcs.js; int &je = indcs.je;
@@ -119,24 +136,35 @@ void ProblemGenerator::UserProblem(ParameterInput *pin, const bool restart) {
   EOS_Data &eos = pmbp->phydro->peos->eos_data;
   Real gm1 = eos.gamma - 1.0;
   auto &profile = profile_reader;
+  auto &disk_profile = disk_profile_reader;
   Real r_c = r_circ;
   Real v_c = v_circ;
 
-  std::cout << "Initializing... " << std::endl;
+  Real G = pmbp->punit->grav_constant();
+  Real r_s = r_scale;
+  Real rho_s = rho_scale;
+  Real m_g = m_gal;
+  Real a_g = a_gal;
+  Real z_g = z_gal;
+  Real r_m = r_200;
+  Real rho_m = rho_mean;
 
   // Use loaded profiles
   par_for("pgen_ic", DevExeSpace(), 0, (pmbp->nmb_thispack-1), ks, ke, js, je, is, ie,
   KOKKOS_LAMBDA(int m, int k, int j, int i) {
-    
     Real &x1min = size.d_view(m).x1min;
     Real &x1max = size.d_view(m).x1max;
     int nx1 = indcs.nx1;
     Real x1v = CellCenterX(i-is, nx1, x1min, x1max);
+    Real x1l =   LeftEdgeX(i-is, nx1, x1min, x1max);
+    Real x1r = LeftEdgeX(i+1-is, nx1, x1min, x1max);
 
     Real &x2min = size.d_view(m).x2min;
     Real &x2max = size.d_view(m).x2max;
     int nx2 = indcs.nx2;
     Real x2v = CellCenterX(j-js, nx2, x2min, x2max);
+    Real x2l =   LeftEdgeX(j-js, nx2, x2min, x2max);
+    Real x2r = LeftEdgeX(j+1-js, nx2, x2min, x2max);
 
     Real &x3min = size.d_view(m).x3min;
     Real &x3max = size.d_view(m).x3max;
@@ -145,11 +173,84 @@ void ProblemGenerator::UserProblem(ParameterInput *pin, const bool restart) {
 
     SetCoolingFlowState(u0, m, k, j, i, x1v, x2v, x3v, gm1, profile);
     SetRotation(u0, m, k, j, i, x1v, x2v, x3v, r_c, v_c);
+    SetEquilibriumState(u0, m, k, j, i, x1v, x2v, x3v, 
+                        x1l, x1r, x2l, x2r, G, r_s, rho_s, 
+                        m_g, a_g, z_g, r_m, rho_m, gm1, disk_profile);
   });
 
-  std::cout << "Initialized. " << std::endl;
-
   return;
+}
+
+KOKKOS_INLINE_FUNCTION
+void SetEquilibriumState(const DvceArray5D<Real> &u0, 
+                         int m, int k, int j, int i, 
+                         Real x1v, Real x2v, Real x3v, 
+                         Real x1l, Real x1r, Real x2l, Real x2r, 
+                         Real G, Real r_s, Real rho_s, Real m_g, 
+                         Real a_g, Real z_g, Real r_m, Real rho_m,
+                         Real gm1, const ProfileReader &disk_profile) {
+    // Calculate radius
+    Real R = sqrt(x1v * x1v + x2v * x2v);
+    // if (R > 3 * a_g or abs(x3v) > 3 * z_g) return;
+
+    Real R1l = sqrt(x1l * x1l + x2v * x2v);
+    Real R1r = sqrt(x1r * x1r + x2v * x2v);
+    Real R2l = sqrt(x1v * x1v + x2l * x2l);
+    Real R2r = sqrt(x1v * x1v + x2r * x2r);
+
+    // Calculate Gravitational Potentials
+    Real phi0    = GravPot(x1v, x2v, 0.0, G, r_s, rho_s, m_g, a_g, z_g, r_m, rho_m);
+    Real phi0_1l = GravPot(x1l, x2v, 0.0, G, r_s, rho_s, m_g, a_g, z_g, r_m, rho_m);
+    Real phi0_1r = GravPot(x1r, x2v, 0.0, G, r_s, rho_s, m_g, a_g, z_g, r_m, rho_m);
+    Real phi0_2l = GravPot(x1v, x2l, 0.0, G, r_s, rho_s, m_g, a_g, z_g, r_m, rho_m);
+    Real phi0_2r = GravPot(x1v, x2r, 0.0, G, r_s, rho_s, m_g, a_g, z_g, r_m, rho_m);
+
+    Real phi   = GravPot(x1v, x2v, x3v, G, r_s, rho_s, m_g, a_g, z_g, r_m, rho_m);
+    Real phi1r = GravPot(x1r, x2v, x3v, G, r_s, rho_s, m_g, a_g, z_g, r_m, rho_m);
+    Real phi2l = GravPot(x1v, x2l, x3v, G, r_s, rho_s, m_g, a_g, z_g, r_m, rho_m);
+    Real phi2r = GravPot(x1v, x2r, x3v, G, r_s, rho_s, m_g, a_g, z_g, r_m, rho_m);
+    Real phi1l = GravPot(x1l, x2v, x3v, G, r_s, rho_s, m_g, a_g, z_g, r_m, rho_m);
+
+    Real f_x1_ = -(phi1r - phi1l) / (x1r - x1l);
+    Real f_x2_ = -(phi2r - phi2l) / (x2r - x2l);
+    Real dPhi_dR = f_x1_ * x1v / R  + f_x2_ * x2v / R;
+
+    // Compute sound speed squared
+    Real temp = disk_profile.GetTemperature(R);
+    Real cs2 = temp; // isothermal, no factor of gamma
+
+    // Get densities from profiles via interpolation
+    Real rho = disk_profile.GetDensity(R) * exp(-(phi - phi0) / cs2);
+    Real rho_1l = disk_profile.GetDensity(R1l) * exp(-(phi1l - phi0_1l) / cs2);
+    Real rho_1r = disk_profile.GetDensity(R1r) * exp(-(phi1r - phi0_1r) / cs2);
+    Real rho_2l = disk_profile.GetDensity(R2l) * exp(-(phi2l - phi0_2l) / cs2);
+    Real rho_2r = disk_profile.GetDensity(R2r) * exp(-(phi2r - phi0_2r) / cs2); 
+    Real p_x1_ = temp*(rho_1r - rho_1l) / (x1r - x1l);
+    Real p_x2_ = temp*(rho_2r - rho_2l) / (x2r - x2l);
+
+    Real dP_dR_over_rho = 0.0;
+    constexpr Real tiny = 1.0e-20;
+    if (rho > tiny) {
+      dP_dR_over_rho = (p_x1_ * x1v  + p_x2_ * x2v) / (R * rho);
+    }
+                      
+    // Calculate circular velocity
+    Real v_phi = sqrt(R*fmax(dP_dR_over_rho - dPhi_dR, 0.0));
+    
+    // Calculate azimuthal velocity
+    Real v1 = 0.0, v2 = 0.0;
+    if (R > tiny) {  // Avoid division by zero     
+      // Calculate azimuthal velocity components
+      v1 = -v_phi * x2v / R;
+      v2 =  v_phi * x1v / R;
+    }
+
+    // Set state variables
+    u0(m, IDN, k, j, i) += rho;
+    u0(m, IM1, k, j, i) += rho * v1;
+    u0(m, IM2, k, j, i) += rho * v2;
+    u0(m, IM3, k, j, i) += 0.0;
+    u0(m, IEN, k, j, i) += (rho * temp)/gm1 + 0.5 * rho * (SQR(v1) + SQR(v2));
 }
 
 KOKKOS_INLINE_FUNCTION
@@ -197,23 +298,23 @@ void SetRotation(const DvceArray5D<Real> &u0,
   
   // Calculate azimuthal velocity
   Real v1 = 0.0, v2 = 0.0, v3 = 0.0;
-  constexpr Real tiny = 1.0e-20;
-  if (r > tiny and R > tiny) {  // Avoid division by zero
-    Real v_phi = 0.0;
-    Real sin_theta = R / r;
+  // constexpr Real tiny = 1.0e-20;
+  // if (r > tiny and R > tiny) {  // Avoid division by zero
+  //   Real v_phi = 0.0;
+  //   Real sin_theta = R / r;
 
-    if (r < r_circ) {
-      v_phi = v_circ * sin_theta;
-    }
-    if (r > r_circ) {
-      v_phi = v_circ * sin_theta * r_circ / r;
-    }
+  //   if (r < r_circ) {
+  //     v_phi = v_circ * sin_theta;
+  //   }
+  //   if (r > r_circ) {
+  //     v_phi = v_circ * sin_theta * r_circ / r;
+  //   }
     
-    // Calculate azimuthal velocity components
-    v1 = -v_phi * x2v / R;
-    v2 = v_phi * x1v / R;
-    v3 = 0.0;
-  }
+  //   // Calculate azimuthal velocity components
+  //   v1 = -v_phi * x2v / R;
+  //   v2 = v_phi * x1v / R;
+  //   v3 = 0.0;
+  // }
   
   // Set state variables
   Real rho = u0(m, IDN, k, j, i);
@@ -229,76 +330,6 @@ void SetRotation(const DvceArray5D<Real> &u0,
 
 void UserSource(Mesh* pm, const Real bdt) {
   GravitySource(pm, bdt);
-  MassLossSource(pm, bdt);
-}
-
-void MassLossSource(Mesh* pm, const Real bdt) {
-  MeshBlockPack *pmbp = pm->pmb_pack;
-  auto &indcs = pm->mb_indcs;
-
-  int is = indcs.is, ie = indcs.ie;
-  int js = indcs.js, je = indcs.je;
-  int ks = indcs.ks, ke = indcs.ke;
-  int nmb1 = pmbp->nmb_thispack - 1;
-  auto &size = pmbp->pmb->mb_size;
-
-  auto &u0 = pmbp->phydro->u0;
-  auto &w0 = pmbp->phydro->w0;
-  auto &eos = pmbp->phydro->peos->eos_data;
-  Real gm1 = eos.gamma - 1.0;
-  auto &profile = profile_reader;
-
-  Real r_c = r_circ;
-  Real v_c = v_circ;
-
-  Real ml_rate = mass_loss_rate;
-  Real ml_radius = mass_loss_radius;
-
-  par_for("user_source", DevExeSpace(), 0, nmb1, ks, ke, js, je, is, ie,
-  KOKKOS_LAMBDA(const int m, const int k, const int j, const int i) {
-    Real &x1min = size.d_view(m).x1min;
-    Real &x1max = size.d_view(m).x1max;
-    int nx1 = indcs.nx1;
-    Real x1v = CellCenterX(i-is, nx1, x1min, x1max);
-
-    Real &x2min = size.d_view(m).x2min;
-    Real &x2max = size.d_view(m).x2max;
-    int nx2 = indcs.nx2;
-    Real x2v = CellCenterX(j-js, nx2, x2min, x2max);
-
-    Real &x3min = size.d_view(m).x3min;
-    Real &x3max = size.d_view(m).x3max;
-    int nx3 = indcs.nx3;
-    Real x3v = CellCenterX(k-ks, nx3, x3min, x3max);
-    
-    Real r = sqrt(x1v*x1v + x2v*x2v + x3v*x3v);
-    if (r < 10.0) {
-      SetCoolingFlowState(u0, m, k, j, i, x1v, x2v, x3v, gm1, profile);
-      SetRotation(u0, m, k, j, i, x1v, x2v, x3v, r_c, v_c);
-    }
-
-    // Real rho = w0(m,IDN,k,j,i);
-    // Real temp = w0(m,IEN,k,j,i)/rho*gm1;
-
-    // if (r < ml_radius) {
-    //   Real mdot = ml_rate/(4./3.*M_PI*ml_radius*ml_radius*ml_radius);
-    //   Real dm = min(bdt*mdot, 0.99*rho);
-    //   u0(m,IDN,k,j,i) -= dm;
-    //   u0(m,IM1,k,j,i) -= dm*w0(m,IVX,k,j,i);
-    //   u0(m,IM2,k,j,i) -= dm*w0(m,IVY,k,j,i);
-    //   u0(m,IM3,k,j,i) -= dm*w0(m,IVZ,k,j,i);
-    //   u0(m,IEN,k,j,i) -= dm*(temp/gm1 + 0.5*(SQR(w0(m,IVX,k,j,i)) +
-    //                                          SQR(w0(m,IVY,k,j,i)) +
-    //                                          SQR(w0(m,IVZ,k,j,i))));
-
-    //   // add some cooling to counteract excessive shock heating at the center
-    //   if (temp>profile.GetTemperature(0.0)) {
-    //     u0(m,IEN,k,j,i) += (profile.GetTemperature(0.0)-temp)*rho/gm1;
-    //   }
-    // }
-  });
-
-  return;
 }
 
 void GravitySource(Mesh* pm, const Real bdt) {
@@ -368,6 +399,12 @@ void GravitySource(Mesh* pm, const Real bdt) {
     u0(m,IEN,k,j,i) += (src_x1*w0(m,IVX,k,j,i) 
                        +src_x2*w0(m,IVY,k,j,i) 
                        +src_x3*w0(m,IVZ,k,j,i));
+
+    // // add some cooling to counteract compressive heating at the center
+    // Real temp = w0(m,IEN,k,j,i)/density*(2.0/3);
+    // if (temp > 1.4) {
+    //   u0(m,IEN,k,j,i) += (1.4-temp)*density/(2.0/3);
+    // }
     
   });
 
@@ -391,9 +428,6 @@ Real GravPot(Real x1, Real x2, Real x3,
   Real x = r / r_s;
   Real phi_NFW = -4 * M_PI * G * rho_s * SQR(r_s) * log(1 + x) / x;
   
-  // Plummer component
-  // Real phi_Plummer = -G * M_gal / sqrt(r2 + SQR(a_gal));
-
   // Miyamoto-Nagai model
   Real phi_MN = -G * M_gal / sqrt(R*R + SQR(sqrt(x3*x3 + z_gal*z_gal) + a_gal));
   
@@ -403,7 +437,6 @@ Real GravPot(Real x1, Real x2, Real x3,
   Real phi_Outer = 4 * M_PI * G * rho_mean * (term1 + term2);
   
   // Total potential
-  // Real phi = phi_NFW + phi_Plummer + phi_Outer;
   Real phi = phi_NFW + phi_MN + phi_Outer;
   
   return phi;
@@ -431,9 +464,21 @@ void UserBoundary(Mesh* pm) {
   auto &u0 = pmbp->phydro->u0;
   auto &eos = pmbp->phydro->peos->eos_data;
   Real gm1 = eos.gamma - 1.0;
+
   auto &profile = profile_reader;
+  // auto &disk_profile = disk_profile_reader;
+
   Real r_c = r_circ;
   Real v_c = v_circ;
+
+  // Real G = pmbp->punit->grav_constant();
+  // Real r_s = r_scale;
+  // Real rho_s = rho_scale;
+  // Real m_g = m_gal;
+  // Real a_g = a_gal;
+  // Real z_g = z_gal;
+  // Real r_m = r_200;
+  // Real rho_m = rho_mean;
 
   // Handle X1 boundaries
   par_for("static_x1", DevExeSpace(), 0, nmb1, 0, (n3-1), 0, (n2-1), 0, (ng-1),
@@ -454,6 +499,8 @@ void UserBoundary(Mesh* pm) {
       
       SetCoolingFlowState(u0, m, k, j, i, x1v, x2v, x3v, gm1, profile);
       SetRotation(u0, m, k, j, i, x1v, x2v, x3v, r_c, v_c);
+      // SetEquilibriumState(u0, m, k, j, i, x1v, x2v, x3v, G, r_s, rho_s, 
+      //                     m_g, a_g, z_g, r_m, rho_m, gm1, disk_profile);
     }
   
     // Outer X1 boundary
@@ -463,6 +510,8 @@ void UserBoundary(Mesh* pm) {
       
       SetCoolingFlowState(u0, m, k, j, i_out, x1v, x2v, x3v, gm1, profile);
       SetRotation(u0, m, k, j, i_out, x1v, x2v, x3v,r_c, v_c);
+      // SetEquilibriumState(u0, m, k, j, i_out, x1v, x2v, x3v, G, r_s, rho_s, 
+      //                     m_g, a_g, z_g, r_m, rho_m, gm1, disk_profile);
     }
   });
   
@@ -485,6 +534,8 @@ void UserBoundary(Mesh* pm) {
       
       SetCoolingFlowState(u0, m, k, j, i, x1v, x2v, x3v, gm1, profile);
       SetRotation(u0, m, k, j, i, x1v, x2v, x3v, r_c, v_c);
+      // SetEquilibriumState(u0, m, k, j, i, x1v, x2v, x3v, G, r_s, rho_s, 
+      //   m_g, a_g, z_g, r_m, rho_m, gm1, disk_profile);
     }
   
     // Outer X2 boundary
@@ -494,6 +545,8 @@ void UserBoundary(Mesh* pm) {
       
       SetCoolingFlowState(u0, m, k, j_out, i, x1v, x2v, x3v, gm1, profile);
       SetRotation(u0, m, k, j_out, i, x1v, x2v, x3v, r_c, v_c);
+      // SetEquilibriumState(u0, m, k, j_out, i, x1v, x2v, x3v, G, r_s, rho_s, 
+      //   m_g, a_g, z_g, r_m, rho_m, gm1, disk_profile);
     }
   });
   
@@ -516,6 +569,8 @@ void UserBoundary(Mesh* pm) {
       
       SetCoolingFlowState(u0, m, k, j, i, x1v, x2v, x3v, gm1, profile);
       SetRotation(u0, m, k, j, i, x1v, x2v, x3v, r_c, v_c);
+      // SetEquilibriumState(u0, m, k, j, i, x1v, x2v, x3v, G, r_s, rho_s, 
+      //   m_g, a_g, z_g, r_m, rho_m, gm1, disk_profile);
     }
     
     // Outer X3 boundary
@@ -525,9 +580,20 @@ void UserBoundary(Mesh* pm) {
       
       SetCoolingFlowState(u0, m, k_out, j, i, x1v, x2v, x3v, gm1, profile);
       SetRotation(u0, m, k_out, j, i, x1v, x2v, x3v, r_c, v_c);
+      // SetEquilibriumState(u0, m, k_out, j, i, x1v, x2v, x3v, G, r_s, rho_s, 
+      //   m_g, a_g, z_g, r_m, rho_m, gm1, disk_profile);
     }
   });
   
+}
+
+//===========================================================================//
+//                              Refinement                                   //
+//===========================================================================//
+
+// User defined refinement
+void RefinementCondition(MeshBlockPack* pmbp) {
+  return; //pmbp->pz4c->pamr->Refine(pmbp);
 }
 
 //===========================================================================//
@@ -537,4 +603,5 @@ void UserBoundary(Mesh* pm) {
 void FreeProfile(ParameterInput *pin, Mesh *pm) {
   // Free Kokkos views before Kokkos::finalize is called
   profile_reader.~ProfileReader();
+  disk_profile_reader.~ProfileReader();
 }
