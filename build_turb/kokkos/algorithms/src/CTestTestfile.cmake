be2322ff53801f15948290f82ee59415de675753
Drummond Fielding <drummondfielding@gmail.com>
2026-02-19 14:22:33 -0500

    getting rid of build_turb

--- build_turb/kokkos/algorithms/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /ccs/home/dfielding/athenak-df/kokkos/algorithms/src
-# Build directory: /ccs/home/dfielding/athenak-df/build_turb/kokkos/algorithms/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build_turb/kokkos/simd/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /ccs/home/dfielding/athenak-df/kokkos/simd/src
-# Build directory: /ccs/home/dfielding/athenak-df/build_turb/kokkos/simd/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
