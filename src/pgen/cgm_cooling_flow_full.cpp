6f2120a936dd64181547ac88270533cc3b2c87e6
Zun Yi Brent Tan <brenttanzunyi@gmail.com>
2026-04-16 15:36:21 -0400

    dust updates

--- src/pgen/cgm_cooling_flow_full.cpp
+++ src/pgen/cgm_cooling_flow_full.cpp
@@ -1019,7 +1019,7 @@ void DustSource(Mesh* pm, const Real bdt) {
       tau_acc_Myr *= (100.0 / nH);
       tau_acc_Myr *= sqrt(50.0 / T_K);
       tau_acc_Myr *= (Z_solar / Z_gas);
-      tau_acc_Myr /= 1.0 - (Z_gas / (Z_gas + D_total));
+      tau_acc_Myr /= 1.0 - (D_total / (Z_gas + D_total));
 
       Real accr_small = 0.0, accr_large = 0.0;
       if (Z_gas > D_floor) {
