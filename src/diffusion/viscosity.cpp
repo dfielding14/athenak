64a641398aa81fa34f83a0ffcad6ab045b757116
Drummond Fielding <drummondfielding@gmail.com>
2026-06-10 15:17:28 -0400

    Remove scratch traffic from viscous fluxes

--- src/diffusion/viscosity.cpp
+++ src/diffusion/viscosity.cpp
@@ -153,7 +153,6 @@ void Viscosity::IsotropicViscousFlux(const DvceArray5D<Real> &w0, const Real nu_
   int is = indcs.is, ie = indcs.ie;
   int js = indcs.js, je = indcs.je;
   int ks = indcs.ks, ke = indcs.ke;
-  int ncells1 = indcs.nx1 + 2*(indcs.ng);
   int nmb1 = pmy_pack->nmb_thispack - 1;
   auto size = pmy_pack->pmb->mb_size;
   bool &multi_d = pmy_pack->pmesh->multi_d;
@@ -170,45 +169,38 @@ void Viscosity::IsotropicViscousFlux(const DvceArray5D<Real> &w0, const Real nu_
   //--------------------------------------------------------------------------------------
   // fluxes in x1-direction
 
-  int scr_level = 0;
-  size_t scr_size = (ScrArray1D<Real>::shmem_size(ncells1)) * 3;
   auto flx1 = flx.x1f;
 
-  par_for_outer("visc1",DevExeSpace(), scr_size, scr_level, 0, nmb1, ks, ke, js, je,
+  par_for_outer("visc1",DevExeSpace(), 0, 0, 0, nmb1, ks, ke, js, je,
   KOKKOS_LAMBDA(TeamMember_t member, const int m, const int k, const int j) {
-    ScrArray1D<Real> fvx(member.team_scratch(scr_level), ncells1);
-    ScrArray1D<Real> fvy(member.team_scratch(scr_level), ncells1);
-    ScrArray1D<Real> fvz(member.team_scratch(scr_level), ncells1);
-
-    // Add [2(dVx/dx)-(2/3)dVx/dx, dVy/dx, dVz/dx]
     par_for_inner(member, is, ie+1, [&](const int i) {
-      fvx(i) = 4.0*(w0(m,IVX,k,j,i) - w0(m,IVX,k,j,i-1))/(3.0*size.d_view(m).dx1);
-      fvy(i) =     (w0(m,IVY,k,j,i) - w0(m,IVY,k,j,i-1))/size.d_view(m).dx1;
-      fvz(i) =     (w0(m,IVZ,k,j,i) - w0(m,IVZ,k,j,i-1))/size.d_view(m).dx1;
-    });
+      // Add [2(dVx/dx)-(2/3)dVx/dx, dVy/dx, dVz/dx]
+      Real fvx = 4.0*(w0(m,IVX,k,j,i) - w0(m,IVX,k,j,i-1))/
+                 (3.0*size.d_view(m).dx1);
+      Real fvy = (w0(m,IVY,k,j,i) - w0(m,IVY,k,j,i-1))/size.d_view(m).dx1;
+      Real fvz = (w0(m,IVZ,k,j,i) - w0(m,IVZ,k,j,i-1))/size.d_view(m).dx1;
 
-    // In 2D/3D Add [(-2/3)dVy/dy, dVx/dy, 0]
-    if (multi_d) {
-      par_for_inner(member, is, ie+1, [&](const int i) {
-        fvx(i) -= ((w0(m,IVY,k,j+1,i) + w0(m,IVY,k,j+1,i-1)) -
-                   (w0(m,IVY,k,j-1,i) + w0(m,IVY,k,j-1,i-1)))/(6.0*size.d_view(m).dx2);
-        fvy(i) += ((w0(m,IVX,k,j+1,i) + w0(m,IVX,k,j+1,i-1)) -
-                   (w0(m,IVX,k,j-1,i) + w0(m,IVX,k,j-1,i-1)))/(4.0*size.d_view(m).dx2);
-      });
-    }
+      // In 2D/3D Add [(-2/3)dVy/dy, dVx/dy, 0]
+      if (multi_d) {
+        fvx -= ((w0(m,IVY,k,j+1,i) + w0(m,IVY,k,j+1,i-1)) -
+                (w0(m,IVY,k,j-1,i) + w0(m,IVY,k,j-1,i-1)))/
+               (6.0*size.d_view(m).dx2);
+        fvy += ((w0(m,IVX,k,j+1,i) + w0(m,IVX,k,j+1,i-1)) -
+                (w0(m,IVX,k,j-1,i) + w0(m,IVX,k,j-1,i-1)))/
+               (4.0*size.d_view(m).dx2);
+      }
 
-    // In 3D Add [(-2/3)dVz/dz, 0,  dVx/dz]
-    if (three_d) {
-      par_for_inner(member, is, ie+1, [&](const int i) {
-        fvx(i) -= ((w0(m,IVZ,k+1,j,i) + w0(m,IVZ,k+1,j,i-1)) -
-                   (w0(m,IVZ,k-1,j,i) + w0(m,IVZ,k-1,j,i-1)))/(6.0*size.d_view(m).dx3);
-        fvz(i) += ((w0(m,IVX,k+1,j,i) + w0(m,IVX,k+1,j,i-1)) -
-                   (w0(m,IVX,k-1,j,i) + w0(m,IVX,k-1,j,i-1)))/(4.0*size.d_view(m).dx3);
-      });
-    }
+      // In 3D Add [(-2/3)dVz/dz, 0,  dVx/dz]
+      if (three_d) {
+        fvx -= ((w0(m,IVZ,k+1,j,i) + w0(m,IVZ,k+1,j,i-1)) -
+                (w0(m,IVZ,k-1,j,i) + w0(m,IVZ,k-1,j,i-1)))/
+               (6.0*size.d_view(m).dx3);
+        fvz += ((w0(m,IVX,k+1,j,i) + w0(m,IVX,k+1,j,i-1)) -
+                (w0(m,IVX,k-1,j,i) + w0(m,IVX,k-1,j,i-1)))/
+               (4.0*size.d_view(m).dx3);
+      }
 
-    // Sum viscous fluxes into fluxes of conserved variables; including energy fluxes
-    par_for_inner(member, is, ie+1, [&](const int i) {
+      // Sum viscous fluxes into fluxes of conserved variables; including energy fluxes
       Real nu = nu_ref;
       if (variable_nu) {
         const Real tl = use_e ? gm1*w0(m,IEN,k,j,i-1)/w0(m,IDN,k,j,i-1)
@@ -219,13 +211,13 @@ void Viscosity::IsotropicViscousFlux(const DvceArray5D<Real> &w0, const Real nu_
                   PowerLawNu(tr, nu_ref, temp_ref, exponent, floor, ceiling));
       }
       Real nud = 0.5*nu*(w0(m,IDN,k,j,i) + w0(m,IDN,k,j,i-1));
-      flx1(m,IVX,k,j,i) -= nud*fvx(i);
-      flx1(m,IVY,k,j,i) -= nud*fvy(i);
-      flx1(m,IVZ,k,j,i) -= nud*fvz(i);
+      flx1(m,IVX,k,j,i) -= nud*fvx;
+      flx1(m,IVY,k,j,i) -= nud*fvy;
+      flx1(m,IVZ,k,j,i) -= nud*fvz;
       if (eos.is_ideal) {
-        flx1(m,IEN,k,j,i) -= 0.5*nud*((w0(m,IVX,k,j,i-1) + w0(m,IVX,k,j,i))*fvx(i) +
-                                      (w0(m,IVY,k,j,i-1) + w0(m,IVY,k,j,i))*fvy(i) +
-                                      (w0(m,IVZ,k,j,i-1) + w0(m,IVZ,k,j,i))*fvz(i));
+        flx1(m,IEN,k,j,i) -= 0.5*nud*((w0(m,IVX,k,j,i-1) + w0(m,IVX,k,j,i))*fvx +
+                                      (w0(m,IVY,k,j,i-1) + w0(m,IVY,k,j,i))*fvy +
+                                      (w0(m,IVZ,k,j,i-1) + w0(m,IVZ,k,j,i))*fvz);
       }
     });
   });
@@ -236,35 +228,32 @@ void Viscosity::IsotropicViscousFlux(const DvceArray5D<Real> &w0, const Real nu_
 
   auto flx2 = flx.x2f;
 
-  par_for_outer("visc2",DevExeSpace(), scr_size, scr_level, 0, nmb1, ks, ke, js, je+1,
+  par_for_outer("visc2",DevExeSpace(), 0, 0, 0, nmb1, ks, ke, js, je+1,
   KOKKOS_LAMBDA(TeamMember_t member, const int m, const int k, const int j) {
-    ScrArray1D<Real> fvx(member.team_scratch(scr_level), ncells1);
-    ScrArray1D<Real> fvy(member.team_scratch(scr_level), ncells1);
-    ScrArray1D<Real> fvz(member.team_scratch(scr_level), ncells1);
-
-    // Add [(dVx/dy+dVy/dx), 2(dVy/dy)-(2/3)(dVx/dx+dVy/dy), dVz/dy]
     par_for_inner(member, is, ie, [&](const int i) {
-      fvx(i) = (w0(m,IVX,k,j,i  ) - w0(m,IVX,k,j-1,i  ))/size.d_view(m).dx2 +
-              ((w0(m,IVY,k,j,i+1) + w0(m,IVY,k,j-1,i+1)) -
-               (w0(m,IVY,k,j,i-1) + w0(m,IVY,k,j-1,i-1)))/(4.0*size.d_view(m).dx1);
-      fvy(i) = (w0(m,IVY,k,j,i) - w0(m,IVY,k,j-1,i))*4.0/(3.0*size.d_view(m).dx2) -
-              ((w0(m,IVX,k,j,i+1) + w0(m,IVX,k,j-1,i+1)) -
-               (w0(m,IVX,k,j,i-1) + w0(m,IVX,k,j-1,i-1)))/(6.0*size.d_view(m).dx1);
-      fvz(i) = (w0(m,IVZ,k,j,i  ) - w0(m,IVZ,k,j-1,i  ))/size.d_view(m).dx2;
-    });
+      // Add [(dVx/dy+dVy/dx), 2(dVy/dy)-(2/3)(dVx/dx+dVy/dy), dVz/dy]
+      Real fvx = (w0(m,IVX,k,j,i  ) - w0(m,IVX,k,j-1,i  ))/size.d_view(m).dx2 +
+                 ((w0(m,IVY,k,j,i+1) + w0(m,IVY,k,j-1,i+1)) -
+                  (w0(m,IVY,k,j,i-1) + w0(m,IVY,k,j-1,i-1)))/
+                 (4.0*size.d_view(m).dx1);
+      Real fvy = (w0(m,IVY,k,j,i) - w0(m,IVY,k,j-1,i))*4.0/
+                 (3.0*size.d_view(m).dx2) -
+                 ((w0(m,IVX,k,j,i+1) + w0(m,IVX,k,j-1,i+1)) -
+                  (w0(m,IVX,k,j,i-1) + w0(m,IVX,k,j-1,i-1)))/
+                 (6.0*size.d_view(m).dx1);
+      Real fvz = (w0(m,IVZ,k,j,i) - w0(m,IVZ,k,j-1,i))/size.d_view(m).dx2;
 
-    // In 3D Add [0, (-2/3)dVz/dz, dVy/dz]
-    if (three_d) {
-      par_for_inner(member, is, ie, [&](const int i) {
-        fvy(i) -= ((w0(m,IVZ,k+1,j,i) + w0(m,IVZ,k+1,j-1,i)) -
-                   (w0(m,IVZ,k-1,j,i) + w0(m,IVZ,k-1,j-1,i)))/(6.0*size.d_view(m).dx3);
-        fvz(i) += ((w0(m,IVY,k+1,j,i) + w0(m,IVY,k+1,j-1,i)) -
-                   (w0(m,IVY,k-1,j,i) + w0(m,IVY,k-1,j-1,i)))/(4.0*size.d_view(m).dx3);
-      });
-    }
+      // In 3D Add [0, (-2/3)dVz/dz, dVy/dz]
+      if (three_d) {
+        fvy -= ((w0(m,IVZ,k+1,j,i) + w0(m,IVZ,k+1,j-1,i)) -
+                (w0(m,IVZ,k-1,j,i) + w0(m,IVZ,k-1,j-1,i)))/
+               (6.0*size.d_view(m).dx3);
+        fvz += ((w0(m,IVY,k+1,j,i) + w0(m,IVY,k+1,j-1,i)) -
+                (w0(m,IVY,k-1,j,i) + w0(m,IVY,k-1,j-1,i)))/
+               (4.0*size.d_view(m).dx3);
+      }
 
-    // Sum viscous fluxes into fluxes of conserved variables; including energy fluxes
-    par_for_inner(member, is, ie, [&](const int i) {
+      // Sum viscous fluxes into fluxes of conserved variables; including energy fluxes
       Real nu = nu_ref;
       if (variable_nu) {
         const Real tl = use_e ? gm1*w0(m,IEN,k,j-1,i)/w0(m,IDN,k,j-1,i)
@@ -275,13 +264,13 @@ void Viscosity::IsotropicViscousFlux(const DvceArray5D<Real> &w0, const Real nu_
                   PowerLawNu(tr, nu_ref, temp_ref, exponent, floor, ceiling));
       }
       Real nud = 0.5*nu*(w0(m,IDN,k,j,i) + w0(m,IDN,k,j-1,i));
-      flx2(m,IVX,k,j,i) -= nud*fvx(i);
-      flx2(m,IVY,k,j,i) -= nud*fvy(i);
-      flx2(m,IVZ,k,j,i) -= nud*fvz(i);
+      flx2(m,IVX,k,j,i) -= nud*fvx;
+      flx2(m,IVY,k,j,i) -= nud*fvy;
+      flx2(m,IVZ,k,j,i) -= nud*fvz;
       if (eos.is_ideal) {
-        flx2(m,IEN,k,j,i) -= 0.5*nud*((w0(m,IVX,k,j-1,i) + w0(m,IVX,k,j,i))*fvx(i) +
-                                      (w0(m,IVY,k,j-1,i) + w0(m,IVY,k,j,i))*fvy(i) +
-                                      (w0(m,IVZ,k,j-1,i) + w0(m,IVZ,k,j,i))*fvz(i));
+        flx2(m,IEN,k,j,i) -= 0.5*nud*((w0(m,IVX,k,j-1,i) + w0(m,IVX,k,j,i))*fvx +
+                                      (w0(m,IVY,k,j-1,i) + w0(m,IVY,k,j,i))*fvy +
+                                      (w0(m,IVZ,k,j-1,i) + w0(m,IVZ,k,j,i))*fvz);
       }
     });
   });
@@ -292,29 +281,29 @@ void Viscosity::IsotropicViscousFlux(const DvceArray5D<Real> &w0, const Real nu_
 
   auto flx3 = flx.x3f;
 
-  par_for_outer("visc3",DevExeSpace(), scr_size, scr_level, 0, nmb1, ks, ke+1, js, je,
+  par_for_outer("visc3",DevExeSpace(), 0, 0, 0, nmb1, ks, ke+1, js, je,
   KOKKOS_LAMBDA(TeamMember_t member, const int m, const int k, const int j) {
-    ScrArray1D<Real> fvx(member.team_scratch(scr_level), ncells1);
-    ScrArray1D<Real> fvy(member.team_scratch(scr_level), ncells1);
-    ScrArray1D<Real> fvz(member.team_scratch(scr_level), ncells1);
-
-    // Add [(dVx/dz+dVz/dx), (dVy/dz+dVz/dy), 2(dVz/dz)-(2/3)(dVx/dx+dVy/dy+dVz/dz)]
     par_for_inner(member, is, ie, [&](const int i) {
-      fvx(i) = (w0(m,IVX,k,j,i  ) - w0(m,IVX,k-1,j,i  ))/size.d_view(m).dx3 +
-              ((w0(m,IVZ,k,j,i+1) + w0(m,IVZ,k-1,j,i+1)) -
-               (w0(m,IVZ,k,j,i-1) + w0(m,IVZ,k-1,j,i-1)))/(4.0*size.d_view(m).dx1);
-      fvy(i) = (w0(m,IVY,k,j,i  ) - w0(m,IVY,k-1,j,i  ))/size.d_view(m).dx3 +
-              ((w0(m,IVZ,k,j+1,i) + w0(m,IVZ,k-1,j+1,i)) -
-               (w0(m,IVZ,k,j-1,i) + w0(m,IVZ,k-1,j-1,i)))/(4.0*size.d_view(m).dx2);
-      fvz(i) = (w0(m,IVZ,k,j,i) - w0(m,IVZ,k-1,j,i))*4.0/(3.0*size.d_view(m).dx3) -
-              ((w0(m,IVX,k,j,i+1) + w0(m,IVX,k-1,j,i+1)) -
-               (w0(m,IVX,k,j,i-1) + w0(m,IVX,k-1,j,i-1)))/(6.0*size.d_view(m).dx1) -
-              ((w0(m,IVY,k,j+1,i) + w0(m,IVY,k-1,j+1,i)) -
-               (w0(m,IVY,k,j-1,i) + w0(m,IVY,k-1,j-1,i)))/(6.0*size.d_view(m).dx2);
-    });
+      // Add [(dVx/dz+dVz/dx), (dVy/dz+dVz/dy),
+      //      2(dVz/dz)-(2/3)(dVx/dx+dVy/dy+dVz/dz)]
+      Real fvx = (w0(m,IVX,k,j,i) - w0(m,IVX,k-1,j,i))/size.d_view(m).dx3 +
+                 ((w0(m,IVZ,k,j,i+1) + w0(m,IVZ,k-1,j,i+1)) -
+                  (w0(m,IVZ,k,j,i-1) + w0(m,IVZ,k-1,j,i-1)))/
+                 (4.0*size.d_view(m).dx1);
+      Real fvy = (w0(m,IVY,k,j,i) - w0(m,IVY,k-1,j,i))/size.d_view(m).dx3 +
+                 ((w0(m,IVZ,k,j+1,i) + w0(m,IVZ,k-1,j+1,i)) -
+                  (w0(m,IVZ,k,j-1,i) + w0(m,IVZ,k-1,j-1,i)))/
+                 (4.0*size.d_view(m).dx2);
+      Real fvz = (w0(m,IVZ,k,j,i) - w0(m,IVZ,k-1,j,i))*4.0/
+                 (3.0*size.d_view(m).dx3) -
+                 ((w0(m,IVX,k,j,i+1) + w0(m,IVX,k-1,j,i+1)) -
+                  (w0(m,IVX,k,j,i-1) + w0(m,IVX,k-1,j,i-1)))/
+                 (6.0*size.d_view(m).dx1) -
+                 ((w0(m,IVY,k,j+1,i) + w0(m,IVY,k-1,j+1,i)) -
+                  (w0(m,IVY,k,j-1,i) + w0(m,IVY,k-1,j-1,i)))/
+                 (6.0*size.d_view(m).dx2);
 
-    // Sum viscous fluxes into fluxes of conserved variables; including energy fluxes
-    par_for_inner(member, is, ie, [&](const int i) {
+      // Sum viscous fluxes into fluxes of conserved variables; including energy fluxes
       Real nu = nu_ref;
       if (variable_nu) {
         const Real tl = use_e ? gm1*w0(m,IEN,k-1,j,i)/w0(m,IDN,k-1,j,i)
@@ -325,13 +314,13 @@ void Viscosity::IsotropicViscousFlux(const DvceArray5D<Real> &w0, const Real nu_
                   PowerLawNu(tr, nu_ref, temp_ref, exponent, floor, ceiling));
       }
       Real nud = 0.5*nu*(w0(m,IDN,k,j,i) + w0(m,IDN,k-1,j,i));
-      flx3(m,IVX,k,j,i) -= nud*fvx(i);
-      flx3(m,IVY,k,j,i) -= nud*fvy(i);
-      flx3(m,IVZ,k,j,i) -= nud*fvz(i);
+      flx3(m,IVX,k,j,i) -= nud*fvx;
+      flx3(m,IVY,k,j,i) -= nud*fvy;
+      flx3(m,IVZ,k,j,i) -= nud*fvz;
       if (eos.is_ideal) {
-        flx3(m,IEN,k,j,i) -= 0.5*nud*((w0(m,IVX,k-1,j,i) + w0(m,IVX,k,j,i))*fvx(i) +
-                                      (w0(m,IVY,k-1,j,i) + w0(m,IVY,k,j,i))*fvy(i) +
-                                      (w0(m,IVZ,k-1,j,i) + w0(m,IVZ,k,j,i))*fvz(i));
+        flx3(m,IEN,k,j,i) -= 0.5*nud*((w0(m,IVX,k-1,j,i) + w0(m,IVX,k,j,i))*fvx +
+                                      (w0(m,IVY,k-1,j,i) + w0(m,IVY,k,j,i))*fvy +
+                                      (w0(m,IVZ,k-1,j,i) + w0(m,IVZ,k,j,i))*fvz);
       }
     });
   });
