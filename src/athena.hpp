5b84c5029cfe2e2e76e26b326b6242e663678a41
Drummond Fielding <drummondfielding@gmail.com>
2026-08-06 12:04:21 -0400

    Use 512-thread teams on Frontier

--- src/athena.hpp
+++ src/athena.hpp
@@ -354,7 +354,11 @@ inline void par_for_outer(const std::string &name, DevExeSpace exec_space,
                           size_t scr_size, const int scr_level,
                           const int kl, const int ku, const Function &function) {
   const int nk = ku - kl + 1;
+#if defined(KOKKOS_ARCH_VEGA90A)
+  Kokkos::TeamPolicy<> policy(exec_space, nk, 512);
+#else
   Kokkos::TeamPolicy<> policy(exec_space, nk, Kokkos::AUTO);
+#endif
   Kokkos::parallel_for(name, policy.set_scratch_size(scr_level,Kokkos::PerTeam(scr_size)),
   KOKKOS_LAMBDA(TeamMember_t tmember) {
     const int k = tmember.league_rank() + kl;
@@ -372,7 +376,11 @@ inline void par_for_outer(const std::string &name, DevExeSpace exec_space,
   const int nk = ku - kl + 1;
   const int nj = ju - jl + 1;
   const int nkj = nk*nj;
+#if defined(KOKKOS_ARCH_VEGA90A)
+  Kokkos::TeamPolicy<> policy(exec_space, nkj, 512);
+#else
   Kokkos::TeamPolicy<> policy(exec_space, nkj, Kokkos::AUTO);
+#endif
   Kokkos::parallel_for(name, policy.set_scratch_size(scr_level,Kokkos::PerTeam(scr_size)),
   KOKKOS_LAMBDA(TeamMember_t tmember) {
     const int k = tmember.league_rank()/nj + kl;
@@ -393,7 +401,11 @@ inline void par_for_outer(const std::string &name, DevExeSpace exec_space,
   const int nj = ju - jl + 1;
   const int nkj  = nk*nj;
   const int nnkj = nn*nk*nj;
+#if defined(KOKKOS_ARCH_VEGA90A)
+  Kokkos::TeamPolicy<> policy(exec_space, nnkj, 512);
+#else
   Kokkos::TeamPolicy<> policy(exec_space, nnkj, Kokkos::AUTO);
+#endif
   Kokkos::parallel_for(name, policy.set_scratch_size(scr_level,Kokkos::PerTeam(scr_size)),
   KOKKOS_LAMBDA(TeamMember_t tmember) {
     int n = (tmember.league_rank())/nkj;
@@ -420,7 +432,11 @@ inline void par_for_outer(const std::string &name, DevExeSpace exec_space,
   const int nkj   = nk*nj;
   const int nnkj  = nn*nk*nj;
   const int nmnkj = nm*nn*nk*nj;
+#if defined(KOKKOS_ARCH_VEGA90A)
+  Kokkos::TeamPolicy<> policy(exec_space, nmnkj, 512);
+#else
   Kokkos::TeamPolicy<> policy(exec_space, nmnkj, Kokkos::AUTO);
+#endif
   Kokkos::parallel_for(name, policy.set_scratch_size(scr_level,Kokkos::PerTeam(scr_size)),
   KOKKOS_LAMBDA(TeamMember_t tmember) {
     int m = (tmember.league_rank())/nnkj;
